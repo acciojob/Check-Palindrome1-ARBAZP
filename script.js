59c3fcb4bdcca1c2cb7f94e8e8dffc15e0baaa07
ARBAZP <117530037+ARBAZP@users.noreply.github.com>
2023-03-15 20:54:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,12 @@
 
 function palindrome(str){
 
+  // Convert the string to lowercase and remove non-alphanumeric characters
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+  // Check if the reversed string is the same as the original string
+  return str === str.split('').reverse().join('');
+
+
 }
 module.exports = palindrome
